558a32a73f72b080aa991c46e564005eb6877ce8
ostrokach <ostrokach@gmail.com>
2017-12-18 14:58:02 -0500

    Refactor fix_column_names

--- pmc_tables/fixers/column_names.py
+++ pmc_tables/fixers/column_names.py
@@ -5,44 +5,35 @@ import pandas as pd
 
 def fix_column_names(df: pd.DataFrame, info: dict) -> pd.DataFrame:
     """Format all column names as strings."""
-    df.columns = _format_columns(df.columns)
-    assert all(isinstance(c, str) for c in df.columns)
+    new_columns: List[str] = []
+    for column_idx, column in enumerate(df.columns):
+        if isinstance(column, (list, tuple)):
+            new_column = ''
+            for col in column:
+                new_col = _format_column(col)
+                if new_col and pd.notnull(new_col):
+                    new_column = new_column + (' | ' if new_column else '') + new_col
+        else:
+            new_column = _format_column(column)
+        new_column = _rename_duplicate(new_column, new_columns)
+        new_columns.append(new_column)
+    df.columns = new_columns
+    assert all(isinstance(c, str) for c in df.columns), df.columns
     return df
 
 
-def _format_columns(columns: List[str]) -> List[str]:
-    """Clean up DataFrame columns."""
-    new_columns: List[str] = []
-    for column_idx, column in enumerate(columns):
-        if isinstance(column, (str, int)):
-            new_column = _format_column(column)
-        elif isinstance(column, (list, tuple)):
-            new_column = ""
-            for col in column:
-                new_col = _format_column(col)
-                if new_col:
-                    new_column += f' | {new_col}' if new_col and pd.notnull(new_col) else ''
-            new_column = new_column.strip('| ')
-        else:
-            new_column = _format_column(str(column))
-        # Make sure there are no duplicates
-        new_column_ref = new_column
-        idx = 0
-        while new_column in new_columns:
-            idx += 1
-            new_column = new_column_ref + '_' + str(idx)
-        # Add new column
-        new_columns.append(new_column)
-    return new_columns
-
-
-def _format_column(col: str) -> str:
-    if isinstance(col, (list, tuple)):
-        return ' / '.join(str(c) for c in col)
-    elif isinstance(col, str):
-        if col.startswith('Unnamed:'):
-            return ''
-        else:
-            return col
+def _format_column(col: object) -> str:
+    col = str(col)
+    if col.startswith('Unnamed:'):
+        return ''
     else:
         return col
+
+
+def _rename_duplicate(column: str, prev_columns: List[str]) -> str:
+    column_ref = column
+    idx = 0
+    while column in prev_columns:
+        idx += 1
+        column = f'{column_ref}_{idx}'
+    return column
