2f9196dd30604871ffb8db8876b03c1c4a8f65ca
ostrokach <ostrokach@gmail.com>
2017-12-18 19:36:32 -0500

    Move get_parser code into __init__.py

--- pmc_tables/parsers/__init__.py
+++ pmc_tables/parsers/__init__.py
@@ -1,4 +1,14 @@
 from .csv import *
 from .excel import *
 from .xml import *
-from .main import *
+
+
+def get_parser(name):
+    if name.endswith('.xml') or name.endswith('.nxml'):
+        return xml_parser
+    elif name.endswith('.xls') or name.endswith('.xlsx'):
+        return excel_parser
+    elif any(name.endswith(ext) for ext in ['.csv', '.csv.gz', '.tsv', '.tsv.gz']):
+        return csv_parser
+    else:
+        raise Exception(f"Do not have an extractor for file {name}")
