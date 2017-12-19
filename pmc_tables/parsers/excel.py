5cd0543d0471b148241e07d618cd6662b7b0c52c
ostrokach <ostrokach@gmail.com>
2017-12-19 10:10:01 -0500

    Open excel files using xlrd and redirect errors to sys.stderr

--- pmc_tables/parsers/excel.py
+++ pmc_tables/parsers/excel.py
@@ -1,14 +1,17 @@
+import sys
 from pathlib import Path
 from typing import List, Tuple
 
 import pandas as pd
+import xlrd
 
 from ._common import parser
 
 
 @parser
 def excel_parser(excel_file: Path) -> List[Tuple[str, dict, pd.DataFrame]]:
-    sheets = pd.read_excel(excel_file.as_posix(), sheet_name=None)
+    wb = xlrd.open_workbook(excel_file.as_posix(), logfile=sys.stderr)
+    sheets = pd.read_excel(wb, sheet_name=None)
     data = []
     for i, (key, df) in enumerate(sheets.items()):
         data.append((f"/{excel_file.name}/sheet_{i}", {'label': key}, df))
